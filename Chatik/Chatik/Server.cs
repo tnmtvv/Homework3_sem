8cba4dca7c9473b10a3c6bd3d5519b3de109bc0c
tnmtvv <62231627+tnmtvv@users.noreply.github.com>
2021-11-09 14:03:23 +0300

    kr_commit

---
+++ Chatik/Chatik/Server.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+
+namespace Chatik
+{
+    class Server
+    {
+        static TcpListener listener;
+        static List<TcpClient> Clients;
+
+        Server(int port)
+        {
+            listener = new TcpListener(IPAddress.Any, port);
+        }
+
+        void Server_work()
+        {
+            listener.Start();
+
+            while (true)
+            {
+                TcpClient client = new TcpClient();
+
+
+            }
+        }
+
+
+
+    }
+}
