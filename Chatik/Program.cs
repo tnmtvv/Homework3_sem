14c5c455b0641a6ce096a264c502d70740587c44
tnmtvv <62231627+tnmtvv@users.noreply.github.com>
2021-11-09 20:38:02 +0300

    commit_try

---
+++ Chatik/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+
+
+namespace Chatik
+{
+    class Program
+    {
+        private static async Task Main(string[] args)
+        {
+           
+
+            if (args.Length > 2)
+            {
+                Console.WriteLine("Two Arguments are required:\n please enter IP adress and a port for Client or just a port ");
+                Environment.Exit(0);
+            }
+            else if (args.Length == 1)
+            {
+                int port;
+                int.TryParse(args[0], out port);
+
+                Server server = new Server(port);
+                await server.Server_work();
+            }
+
+            else if (args.Length == 2)
+            {
+                IPAddress.TryParse(args[0], out IPAddress IP);
+                int.TryParse(args[1], out int port);
+
+                Console.WriteLine("Enter your name");
+
+                string name = Console.ReadLine();
+
+                MyClient client = new MyClient(port, IP, name);
+                await client.Start();
+            }
+            
+
+
+        }
+    }
+}
